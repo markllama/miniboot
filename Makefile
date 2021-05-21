45d5be46588467b06b82642a6d23579d555134cf
Mark Lamourine <markllama@gmail.com>
2021-05-21 03:25:22 +0000

    ignore coreos binaries and images

--- Makefile
+++ Makefile
@@ -3,9 +3,10 @@
 IMAGE_NAME=miniboot
 COREOS_VERSION=33.20210426.3.0
 COREOS_KERNEL=fedora-coreos-$(COREOS_VERSION)-live-kernel-x86_64
-COREOS_RAMDISK=fedora-coreos-$(COREOS_VERSION)-live-initramfs.x86_64.img
+COREOS_INITFS=fedora-coreos-$(COREOS_VERSION)-live-initramfs.x86_64.img
+COREOS_ROOTFS=fedora-coreos-$(COREOS_VERSION)-live-rootfs.x86_64.img
 
-build: $(COREOS_KERNEL) $(COREOS_RAMDISK)
+build: coreos/$(COREOS_KERNEL) coreos/$(COREOS_INITFS) coreos/$(COREOS_ROOTFS)
 	buildah unshare ./build.sh $(IMAGE_NAME)
 
 clean:
@@ -13,14 +14,21 @@ clean:
 	-podman rmi ${IMAGE_NAME}
 
 clean_all: clean
-	rm fedora-coreos*
+	rm -rf coreos
 
 run:
-	podman run -it --rm --name miniboot localhost/miniboot /bin/bash
+	podman run -it --rm --privileged --name miniboot --net=host localhost/miniboot /bin/bash
 
-$(COREOS_KERNEL):
+stop:
+	-podman stop miniboot
+	-podman rm miniboot
+
+coreos/$(COREOS_KERNEL):
+	mkdir -p coreos
+	cd coreos ; \
 	podman run --privileged --pull=always --rm -v .:/data -w /data \
 	  quay.io/coreos/coreos-installer:release download -f pxe
 
-$(COREOS_RAMDISK): $(COREOS_KERNEL)
+coreos/$(COREOS_INITFS): coreos/$(COREOS_KERNEL)
+coreos/$(COREOS_ROOTFS): coreos/$(COREOS_KERNEL)
 
--- build.sh
+++ build.sh
@@ -2,45 +2,59 @@
 #
 # This script uses buildah to compose a container image with a DHCP server
 #
-set -x
+#set -x
 
 CONTAINER_NAME=miniboot
 BASE_IMAGE=registry.access.redhat.com/ubi8/ubi
 #BASE_IMAGE=fedora:34
-COREOS_VERSION=33.20210426.3.0
+COREOS_VERSION=34.20210427.3.0
 COREOS_KERNEL=fedora-coreos-${COREOS_VERSION}-live-kernel-x86_64
 COREOS_INITRD=fedora-coreos-${COREOS_VERSION}-live-initramfs.x86_64.img
 COREOS_ROOTRD=fedora-coreos-${COREOS_VERSION}-live-rootfs.x86_64.img
 
 MAINTAINER="Mark Lamourine <markllama@gmail.com>"
 
-REPO_RPMS=(https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm)
-RPMS=(dhcp-server tftp-server syslinux-tftpboot lighttpd)
+REPO_RPMS=https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+RPMS=(iproute dhcp-server tftp-server syslinux-tftpboot lighttpd python3-pip python3-jinja2)
 SERVICES=(dhcpd tftp-server lighttpd)
 MOUNTPOINTS=(/etc/dhcpd/dhcpd.conf /etc/lighttpd/lighttpd.conf)
-
 
 buildah from --name ${CONTAINER_NAME} ${BASE_IMAGE}
 buildah config --label maintainer="${MAINTAINER}" ${CONTAINER_NAME}
 
-buildah run ${CONTAINER_NAME} dnf -y install dhcp-server
-buildah run ${CONTAINER_NAME} dnf -y install tftp-server
-buildah run ${CONTAINER_NAME} dnf -y install syslinux-tftpboot
+# Configuration files needed:
+# /etc/dhcp/dhcpd.conf - assign IP address, provide next-server and filename
+# /var/lib/tftpboot/pxelinux.cfg/default - provide kernel URL, and kernel boot line
+# /var/www/lighttpd/coreos/config.ign - provide host configuration
+
+#buildah config --volume /etc/dhcp ${CONTAINER_NAME}
+#buildah config --volume /var/lib/tftpboot/pxelinux.cfg ${CONTAINER_NAME}
+#buildah config --volume /var/www/lighttpd/coreos ${CONTAINER_NAME}
+
+declare -a PORTS=(68/tcp 68/udp 69/tcp 69/udp 8069/udp)
+for PORT in ${PORTS[@]} ; do
+    buildah config --port ${PORT} ${CONTAINER_NAME}
+done
+
+buildah run ${CONTAINER_NAME} dnf -y install ${REPO_RPMS}
+
+for RPM in ${RPMS[@]} ; do
+    buildah run ${CONTAINER_NAME} dnf -y install ${RPM}
+done
+
+buildah run ${CONTAINER_NAME} pip3 install jinja2-cli
 
 buildah run ${CONTAINER_NAME} cp /tftpboot/{pxelinux.0,ldlinux.c32} /var/lib/tftpboot
 buildah run ${CONTAINER_NAME} mkdir -p /var/lib/tftpboot/pxelinux.cfg
 buildah run ${CONTAINER_NAME} dnf -y remove syslinux-tftpboot
 
 buildah run ${CONTAINER_NAME} mkdir -p /var/lib/tftpboot/coreos
-buildah copy ${CONTAINER_NAME} ${COREOS_KERNEL} /var/lib/tftpboot/coreos/kernel
-buildah copy ${CONTAINER_NAME} ${COREOS_INITRD} /var/lib/tftpboot/coreos/initrd
-buildah copy ${CONTAINER_NAME} ${COREOS_ROOTRD} /var/lib/tftpboot/coreos/rootrd
-
-buildah run ${CONTAINER_NAME} dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-buildah run ${CONTAINER_NAME} dnf -y install lighttpd
+buildah copy ${CONTAINER_NAME} coreos/${COREOS_KERNEL} /var/lib/tftpboot/coreos/kernel
+buildah copy ${CONTAINER_NAME} coreos/${COREOS_INITRD} /var/lib/tftpboot/coreos/initrd
+buildah copy ${CONTAINER_NAME} coreos/${COREOS_ROOTRD} /var/lib/tftpboot/coreos/rootrd
 
 buildah run ${CONTAINER_NAME} systemctl enable dhcpd
-buildah run ${CONTAINER_NAME} systemctl enable tftp-server
+buildah run ${CONTAINER_NAME} systemctl enable tftp
 buildah run ${CONTAINER_NAME} systemctl enable lighttpd
 
 buildah run ${CONTAINER_NAME} dnf clean all
@@ -48,6 +62,8 @@ buildah run ${CONTAINER_NAME} dnf clean all
 ##
 ## Create config file locations or accept inputs to start
 ##
+buildah copy ${CONTAINER_NAME} templates /opt/templates
+
 # dhcpd.conf
 # /var/lib/tftpboot
 
