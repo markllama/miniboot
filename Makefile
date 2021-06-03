a69ed9313b20888e586dfa6b610c4641ec7469f8
Mark Lamourine <markllama@gmail.com>
2021-06-03 22:43:07 +0000

    more stuff using ipxe

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 #!make
 
 IMAGE_NAME=miniboot
-COREOS_VERSION=33.20210426.3.0
+#COREOS_VERSION=34.20210427.3.0
 COREOS_KERNEL=fedora-coreos-$(COREOS_VERSION)-live-kernel-x86_64
 COREOS_INITFS=fedora-coreos-$(COREOS_VERSION)-live-initramfs.x86_64.img
 COREOS_ROOTFS=fedora-coreos-$(COREOS_VERSION)-live-rootfs.x86_64.img
 
-build: coreos/$(COREOS_KERNEL) coreos/$(COREOS_INITFS) coreos/$(COREOS_ROOTFS)
+build: coreos
 	buildah unshare ./build.sh $(IMAGE_NAME)
 
 clean:
@@ -16,18 +16,32 @@ clean:
 clean_all: clean
 	rm -rf coreos
 
+cli:
+	podman run -it --rm --privileged --name miniboot --net=host \
+	  --volume $(PWD)/test:/opt/config \
+	  --volume $(PWD)/coreos:/var/lib/tftpboot/coreos \
+	  --entrypoint=/bin/bash \
+	  localhost/miniboot
+
 run:
-	podman run -it --rm --privileged --name miniboot --net=host --volume $(PWD)/test:/opt/config localhost/miniboot /bin/bash
+	podman run -d --rm --privileged --name miniboot --net=host \
+	  --volume $(PWD)/test:/opt/config \
+	  --volume $(PWD)/coreos:/var/lib/tftpboot/coreos \
+	  localhost/miniboot
 
 stop:
 	-podman stop miniboot
 	-podman rm miniboot
 
-coreos/$(COREOS_KERNEL):
+coreos:
 	mkdir -p coreos
 	cd coreos ; \
 	podman run --privileged --pull=always --rm -v .:/data -w /data \
-	  quay.io/coreos/coreos-installer:release download -f pxe
+	  quay.io/coreos/coreos-installer:release download -f pxe ; \
+	ln -s *kernel-x86_64 kernel ; \
+	ln -s *initramfs.x86_64.img initrd ; \
+	ln -s *rootfs.x86_64.img rootfs
+
 
 coreos/$(COREOS_INITFS): coreos/$(COREOS_KERNEL)
 coreos/$(COREOS_ROOTFS): coreos/$(COREOS_KERNEL)
--- build.sh
+++ build.sh
@@ -7,15 +7,15 @@
 CONTAINER_NAME=miniboot
 BASE_IMAGE=registry.access.redhat.com/ubi8/ubi
 #BASE_IMAGE=fedora:34
-COREOS_VERSION=34.20210427.3.0
-COREOS_KERNEL=fedora-coreos-${COREOS_VERSION}-live-kernel-x86_64
-COREOS_INITRD=fedora-coreos-${COREOS_VERSION}-live-initramfs.x86_64.img
-COREOS_ROOTRD=fedora-coreos-${COREOS_VERSION}-live-rootfs.x86_64.img
+#COREOS_VERSION=34.20210427.3.0
+#COREOS_KERNEL=fedora-coreos-${COREOS_VERSION}-live-kernel-x86_64
+#COREOS_INITRD=fedora-coreos-${COREOS_VERSION}-live-initramfs.x86_64.img
+#COREOS_ROOTRD=fedora-coreos-${COREOS_VERSION}-live-rootfs.x86_64.img
 
 MAINTAINER="Mark Lamourine <markllama@gmail.com>"
 
 REPO_RPMS=https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-RPMS=(iproute dhcp-server tftp-server syslinux-tftpboot lighttpd python3-pip python3-jinja2 python3-pyyaml)
+RPMS=(iproute dhcp-server tftp-server syslinux-tftpboot lighttpd python3-pip python3-jinja2 python3-pyyaml tcpdump)
 SERVICES=(dhcpd tftp-server lighttpd)
 MOUNTPOINTS=(/etc/dhcpd/dhcpd.conf /etc/lighttpd/lighttpd.conf)
 
@@ -27,11 +27,11 @@ buildah config --label maintainer="${MAINTAINER}" ${CONTAINER_NAME}
 # /var/lib/tftpboot/pxelinux.cfg/default - provide kernel URL, and kernel boot line
 # /var/www/lighttpd/coreos/config.ign - provide host configuration
 
-#buildah config --volume /etc/dhcp ${CONTAINER_NAME}
-#buildah config --volume /var/lib/tftpboot/pxelinux.cfg ${CONTAINER_NAME}
-#buildah config --volume /var/www/lighttpd/coreos ${CONTAINER_NAME}
+buildah config --volume /etc/dhcp ${CONTAINER_NAME}
+buildah config --volume /var/lib/tftpboot/coreos ${CONTAINER_NAME}
+buildah config --volume /var/www/lighttpd/ignition ${CONTAINER_NAME}
 
-declare -a PORTS=(68/tcp 68/udp 69/tcp 69/udp 8069/udp)
+declare -a PORTS=(68/tcp 68/udp 69/tcp 69/udp 80/tcp)
 for PORT in ${PORTS[@]} ; do
     buildah config --port ${PORT} ${CONTAINER_NAME}
 done
@@ -44,17 +44,15 @@ done
 
 buildah run ${CONTAINER_NAME} pip3 install jinja2-cli
 
-buildah run ${CONTAINER_NAME} curl -L -o /usr/local/bin/butane \
-        https://github.com/coreos/butane/releases/download/v0.11.0/butane-x86_64-unknown-linux-gnu
-
-buildah run ${CONTAINER_NAME} cp /tftpboot/{pxelinux.0,ldlinux.c32} /var/lib/tftpboot
-buildah run ${CONTAINER_NAME} mkdir -p /var/lib/tftpboot/pxelinux.cfg
+buildah copy ${CONTAINER_NAME} bin/undionly.kpxe /var/lib/tftpboot
+#buildah run ${CONTAINER_NAME} cp /tftpboot/{pxelinux.0,ldlinux.c32} /var/lib/tftpboot
+#buildah run ${CONTAINER_NAME} mkdir -p /var/lib/tftpboot/pxelinux.cfg
 buildah run ${CONTAINER_NAME} dnf -y remove syslinux-tftpboot
 
 buildah run ${CONTAINER_NAME} mkdir -p /var/lib/tftpboot/coreos
-buildah copy ${CONTAINER_NAME} coreos/${COREOS_KERNEL} /var/lib/tftpboot/coreos/kernel
-buildah copy ${CONTAINER_NAME} coreos/${COREOS_INITRD} /var/lib/tftpboot/coreos/initrd
-buildah copy ${CONTAINER_NAME} coreos/${COREOS_ROOTRD} /var/lib/tftpboot/coreos/rootrd
+#buildah copy ${CONTAINER_NAME} coreos/${COREOS_KERNEL} /var/lib/tftpboot/coreos/kernel
+#buildah copy ${CONTAINER_NAME} coreos/${COREOS_INITRD} /var/lib/tftpboot/coreos/initrd
+#buildah copy ${CONTAINER_NAME} coreos/${COREOS_ROOTRD} /var/lib/tftpboot/coreos/rootrd
 
 buildah run ${CONTAINER_NAME} systemctl enable dhcpd
 buildah run ${CONTAINER_NAME} systemctl enable tftp
@@ -71,6 +69,10 @@ buildah config --volume /opt/config ${CONTAINER_NAME}
 
 # dhcpd.conf
 # /var/lib/tftpboot
+
+buildah copy ${CONTAINER_NAME} startup.sh /opt/startup.sh
+buildah run ${CONTAINER_NAME} chmod 755 /opt/startup.sh
+buildah config --cmd /opt/startup.sh ${CONTAINER_NAME}
 
 buildah commit ${CONTAINER_NAME} miniboot
 
