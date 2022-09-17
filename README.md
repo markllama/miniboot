df14296f4f547be1ab2f0d8a2c91fb1d037c54e7
Mark Lamourine <markllama@gmail.com>
2022-09-17 19:02:28 -0400

    start a workflow to build a new container

--- README.md
+++ README.md
@@ -2,11 +2,10 @@
 
 Miniboot is a software container that helps boot single host via PXE
 
-It includes a DHCP server, a TFTP server and the PXELINUX files needed to boot
-a memory resident Linux on a single host.
-
-The user provides the network parameters for the DHCP server and the file URLs
-for the Linux kernel and ramdisk. 
+It includes a DHCP server, a TFTP server and the iPXE files to needed
+to initiate a boot/install on a PXE enabled server. The user must
+provides the server configuration files and the PXE boot payload when
+the container is instantiated.
 
 ## Building the Container Image
 
@@ -15,77 +14,6 @@ for the Linux kernel and ramdisk.
 * interface
 
 * http.port
-
-### Network Parameters
-
-### Host Definitions
-
-* hostname
-* mac_address
-* ipv4_address
-
-### User Definitions
-
-* username
-* password_hash
-* public_key
-
-### Example Config File
-
-~~~
----
-# The name of the interface to listen on
-interface: br-data
-
-# The HTTP port used once iPXE is launched
-http:
-  port: 8080
-
-# Optional, if observing or logging in on the serial console
-console: ttyS1,115200n8
-
-# A list of MAC/IP address pairs for the clients to be booted 
-# - The IP addresses must be in an IP range on the interface above
-clients: 
-  - hostname:     node1
-    mac_address:  00:30:48:9F:C2:02    # MAC address to respond to
-    ipv4_address: 172.18.0.21          # IP address to assign
-
-  - hostname:     node2
-    mac_address:  00:25:90:6a:75:b4    # MAC address to respond to
-    ipv4_address: 172.18.0.22          # IP address to assign
-
-  - hostname:     node3
-    mac_address:  00:25:90:60:b6:14    # MAC address to respond to
-    ipv4_address: 172.18.0.23          # IP address to assign
-# ...
-
-users:
- - name: core
-   # mkpasswd --method=sha256crypt
-   # cleartext: core - replace with your value if desired
-   password_hash: "$1$ebTQlHxO$unj0Wz5STH9.tMn1yFWYE0"
-   # SSH public key
-   public_key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJDsdnCbVpUYOiqvjys/Ub4VP7Kpe7X98MIUREygC+9Q user@example.com"
-~~~
-
-## Generating Configuration Files
-
-### `/etc` files
-
-#### `dhcpd.conf`
-
-#### `thttpd.conf`
-
-### Boot Configuration Files
-
-#### iPXE Configuration Files
-
-#### OS Configuration Files
-
-### CoreOS Binaries
-
-
 
 ## References
 
--- build.sh
+++ build.sh
@@ -2,70 +2,95 @@
 #
 # This script uses buildah to compose a container image with a DHCP server
 #
-set -x
+MAINTAINER="Mark Lamourine <markllama@gmail.com>"
 
 CONTAINER_NAME=$1
+IMAGE_NAME=$2
 
 #BASE_IMAGE=registry.access.redhat.com/ubi8/ubi
 BASE_IMAGE=registry.fedoraproject.org/fedora-minimal:36
 DNF=microdnf
-IMAGE_NAME=$2
 
-MAINTAINER="Mark Lamourine <markllama@gmail.com>"
-
-CURL_OPTS="--location --remote-name --output-dir /tmp"
 RPMS=(systemd dhcp-server tftp-server thttpd)
 SERVICES=(dhcpd tftp thttpd)
-
 PXE_BINARIES=(ldlinux.c32  lpxelinux.0  memdisk  pxelinux.0)
 
+function main() {
+    set_container_metadata
+    install_and_configure_systemd_services
+    populate_pxelinux_binaries
+    populate_ipxe_binaries
+    finalize_container_image
+}
+
+
+# A shortcut for commands that run inside the container
 function buildah_run() {
     buildah run ${CONTAINER_NAME} $*
 }
 
+# This function replaces a file or directory in the container with a link to a 
+# location in an imported volume
 function replace_with_link() {
     local FILE=$1
     local LINK=$2
 
-    buildah run ${CONTAINER_NAME} rm -rf ${FILE}
-    buildah run ${CONTAINER_NAME} ln -s ${LINK} ${FILE}
+    buildah_run rm -rf ${FILE}
+    buildah_run ln -s ${LINK} ${FILE}
 }
 
 # ==================================================================================
 #
 # ==================================================================================
 
-buildah from --name ${CONTAINER_NAME} ${BASE_IMAGE}
-buildah config --label maintainer="${MAINTAINER}" ${CONTAINER_NAME}
+function set_container_metadata() {   
+    # Set container metadata
+    buildah from --name ${CONTAINER_NAME} ${BASE_IMAGE}
+    buildah config --label maintainer="${MAINTAINER}" ${CONTAINER_NAME}
+    buildah config --volume /data ${CONTAINER_NAME}
+}
 
-buildah_run ${DNF} -y install ${RPMS[@]}
+function install_and_configure_systemd_services() {
+    # Install service packages
+    buildah_run ${DNF} -y install ${RPMS[@]}
 
-# Set up PXE boot in TFTP server
-buildah_run ${DNF} -y install syslinux-tftpboot
-for PXE_BIN in ${PXE_BINARIES[@]} ; do
-    buildah_run cp /tftpboot/${PXE_BIN} /var/lib/tftpboot
-done
+    # All of the input is mounted on /data
+    # Replace the stock config files with symlinks to the import directory: /opt
+    # /etc/dhcp/dhcpd.conf -> /data/dhcpd.conf
+    replace_with_link /etc/dhcp/dhcpd.conf /opt/etc/dhcpd.conf 
+    replace_with_link /etc/thttpd.conf /opt/etc/thttpd.conf
+    replace_with_link /var/www/thttpd /opt/www
+    
+    # Enable services inside the container
+    buildah_run systemctl enable ${SERVICES[@]}
+    # Start with systemd
+    buildah config --cmd '["/usr/sbin/init"]' ${CONTAINER_NAME} 
+}
 
-# Try ipxe instead
-buildah add ${CONTAINER_NAME} ipxe/src/bin/undionly.kpxe /var/lib/tftpboot/undionly.kpxe
-buildah add ${CONTAINER_NAME} ipxe.efi /var/lib/tftpboot/ipxe.efi
+function populate_pxelinux_binaries() {
+    # Populate the PXELINUX boot files in the TFTP repository
+    buildah_run ${DNF} -y install syslinux-tftpboot
+    for PXE_BIN in ${PXE_BINARIES[@]} ; do
+        buildah_run cp /tftpboot/${PXE_BIN} /var/lib/tftpboot
+    done
+    buildah_run ${DNF} -y remove syslinux-tftpboot
+    buildah_run ${DNF} clean all
+}
 
-buildah_run ${DNF} -y remove syslinux-tftpboot
+function populate_ipxe_binaries() {
+    # Place iPXE boot files in the TFTP repository
+    buildah add ${CONTAINER_NAME} ipxe/src/bin/undionly.kpxe /var/lib/tftpboot/undionly.kpxe
+    buildah add ${CONTAINER_NAME} ipxe.efi /var/lib/tftpboot/ipxe.efi
+}
 
-buildah_run ${DNF} clean all
+function finalize_container_image() {
+    #
+    # Finalize the new container image
+    #
+    buildah commit ${CONTAINER_NAME} ${IMAGE_NAME}
+    buildah unmount ${CONTAINER_NAME}
+}
 
-buildah_run systemctl enable ${SERVICES[@]}
-
-buildah config --volume /data ${CONTAINER_NAME}
-
-# All of the input is mounted on /data
-# /etc/dhcp/dhcpd.conf -> /data/dhcpd.conf
-replace_with_link /etc/dhcp/dhcpd.conf /opt/etc/dhcpd.conf 
-replace_with_link /etc/thttpd.conf /opt/etc/thttpd.conf
-replace_with_link /var/www/thttpd /opt/www
-
-buildah config --cmd '["/usr/sbin/init"]' ${CONTAINER_NAME} 
-
-buildah commit ${CONTAINER_NAME} ${IMAGE_NAME}
-
-buildah unmount ${CONTAINER_NAME}
+# ----------------------------------------------------------------------------------------------
+# Execute the main function
+main $*
